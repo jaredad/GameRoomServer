d2cf9eddd7afb13c92cbf16cda2f685b51f048f2
jaredad <35081849+jaredad@users.noreply.github.com>
2018-04-30 04:02:32 -0500

    Add files via upload

--- src/profile/Database.java
+++ src/profile/Database.java
@@ -1,11 +1,17 @@
 package profile;
 
 import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Database {
 
 	private Statement stat;
 	private Connection con;
+	
+	public Database() throws SQLException, ClassNotFoundException {
+		getLargestId();
+	}
 	
 	public void openConnection() throws ClassNotFoundException, SQLException {
 		Class.forName("org.sqlite.JDBC");
@@ -13,8 +19,48 @@ public class Database {
 		stat = con.createStatement();
 	}
 	
+	public void addNewProfile(List<String> profile) throws ClassNotFoundException, SQLException {
+		int id = setId(profile.get(0));
+		openConnection();
+		String cmd = "INSERT INTO all_players (id, name, ip, birth, bio, image)"
+				+ " VALUES('" + id + "', '" + profile.get(1) + "', '" + profile.get(2) + "', '"
+				+ profile.get(3) + "', '" + profile.get(4) + "', '" + profile.get(5) + "')";
+		stat.execute(cmd);
+		con.close();
+	}
 	
+	public int setId(String id) throws ClassNotFoundException, SQLException {
+		int id_num = 0;
+		if (id.equals("0")) {
+			id_num = getLargestId()+1;
+		} else {
+			id_num = Integer.parseInt(id);
+		}
+		return id_num;
+	}
 	
+	public int getLargestId() throws ClassNotFoundException, SQLException {
+		openConnection();
+		int id = 0;
+		String cmd = "SELECT MAX(id) FROM all_players";
+		stat.execute(cmd);
+		ResultSet results = stat.getResultSet();
+		id = results.getInt(1);
+		return id;
+	}
+	
+	public void getTableValues() throws SQLException, ClassNotFoundException {
+		openConnection();
+		String cmd = "Select * From all_players;";
+		stat.execute(cmd);
+		ResultSet results = stat.getResultSet();
+		while (results.next()) {
+			for (int c = 1; c <= 6; ++c) {
+				System.out.println(results.getString(c));
+			}
+		}
+		con.close();
+	}
 	
 	
 }
